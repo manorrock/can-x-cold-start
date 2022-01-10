96a621cd67066fc6792f265215eb8df0d34af0f6
Manfred Riem <mriem@manorrock.com>
2022-01-10 11:54:04 -0700

    Fixes issue #18 - Add Glassfish

---
+++ glassfish/src/main/java/coldstart/glassfish/HelloWorldServlet.java
@@ -0,0 +1,66 @@
+package coldstart.glassfish;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.FileWriter;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = {""})
+public class HelloWorldServlet extends HttpServlet {
+
+    private String outputFilename;
+
+    private long time1;
+
+    private long time2;
+
+    private long time3;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        time2 = System.currentTimeMillis();
+        outputFilename = System.getenv("OUTPUT_FILENAME");
+    }
+
+    @Override
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/plain");
+        try ( PrintWriter out = response.getWriter()) {
+            out.println("Hello World!");
+        }
+        time3 = System.currentTimeMillis();
+        writeResults(request.getServletContext());
+    }
+
+    /**
+     * Write the results asynchronously.
+     *
+     * @param context the servlet context.
+     */
+    public void writeResults(ServletContext context) {
+        time1 = (long) context.getAttribute("TIME1");
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+                try ( PrintWriter writer = new PrintWriter(new FileWriter(outputFilename, true))) {
+                    writer.println(time1);
+                    writer.println(time2);
+                    writer.println(time3);
+                } catch (IOException ioe) {
+                    // nothing to do here.
+                }
+            }
+        };
+        thread.start();
+    }
+}
---
+++ glassfish/src/main/java/coldstart/glassfish/HelloWorldServletContainerInitializer.java
@@ -0,0 +1,14 @@
+package coldstart.glassfish;
+
+import java.util.Set;
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
+public class HelloWorldServletContainerInitializer implements ServletContainerInitializer {
+
+    @Override
+    public void onStartup(Set<Class<?>> set, ServletContext sc) throws ServletException {
+        sc.setAttribute("TIME1", System.currentTimeMillis());
+    }
+}
